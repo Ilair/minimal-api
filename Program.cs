aad1fcb3379d97e0cc27ffc787788dfd77d20774
Ilair dos Santos <ilair.santos@gmail.com>
2024-08-20 20:37:26 -0300

    Alterado o banco para acessar o Sql Server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using minimal_api.DTOs;
+using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("sqlserver")
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -14,4 +23,4 @@ app.MapPost("/login", (LoginDTO loginDTO) =>{
 
 app.Run();
 
-    
+    //parei no vídeo Criando Seed para cadastrar administrador padrão
